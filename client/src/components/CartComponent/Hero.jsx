c73aa4e9c93876fed0b3a96c6c606efe45d0305e
Kamran <kamrankhan815.com@gmail.com>
2025-03-25 16:40:21 +0500

    add cart page

---
+++ client/src/components/CartComponent/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import img1 from "../../assets/images/storeImages/img1.jpg";
+import { Link } from "react-router-dom";
+
+const Hero = () => {
+  return (
+    <div>
+      <div className="image h-[60vh] bg-gray-100 py-10 relative">
+        <img src={img1} className="w-full h-full object-cover" alt="" />
+        <div className="shop absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] flex items-center justify-center flex-col space-y-2">
+          <h1 className="font-['Playfair_Display'] font-bold text-4xl">My Cart</h1>
+          <p className="text-xl">
+            <Link to={"/"}>
+              <span className="hover:text-green-600 cursor-pointer">Home </span>
+            </Link>
+            /{" "}
+            <Link to={"/store"}>
+              <span className="hover:text-green-600 cursor-pointer">Shop </span>
+            </Link>{" "}
+            / <span className="text-green-600">My Cart</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- client/src/pages/Page/Page.jsx
+++ client/src/pages/Page/Page.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import Hero from '../../components/CartComponent/Hero';
+import Cart from '../../components/CartComponent/Cart';
+import NewsLetter from '../../components/NewsLetter/NewsLetter';
 
 const Page = () => {
   return (
-    <div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum vero amet debitis sequi nulla tenetur doloribus aperiam ut, eaque reprehenderit? Molestias ipsam ea praesentium iusto minima, repellat animi dolorem unde. Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit reiciendis, illum tempore facilis assumenda aperiam itaque autem? Unde quis quisquam, molestiae sit numquam modi commodi, sint ad ipsa laudantium eius.</p>
+    <div className="space-y-20 [@media(max-width:576px)]:px-[15px] px-[50px] md:px-[60px] lg:px-[70px]">
+      <div>
+        <Hero />
+      </div>
+      <div>
+        <Cart />
+      </div>
+      <div>
+       <NewsLetter />
+      </div>
     </div>
   );
 }
