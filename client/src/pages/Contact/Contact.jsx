00a220e15c5476ca90f7e29972febeb7b003a886
Kamran <kamrankhan815.com@gmail.com>
2025-03-25 16:46:10 +0500

    start the contact page

--- client/src/pages/Contact/Contact.jsx
+++ client/src/pages/Contact/Contact.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Hero from '../../components/ContactComponent/Hero';
 
 const Contact = () => {
   return (
     <div>
-      Contact
+      <div>
+        <Hero />
+      </div>
     </div>
   );
 }
