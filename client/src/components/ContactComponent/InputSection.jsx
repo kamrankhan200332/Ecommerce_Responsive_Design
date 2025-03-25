f70047867a217525d9c5e27a1ab33c9271893816
Kamran <kamrankhan815.com@gmail.com>
2025-03-25 19:14:03 +0500

    add input section

---
+++ client/src/components/ContactComponent/InputSection.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const InputSection = () => {
+  return (
+    <div className='space-y-10'>
+      <div className="flex items-center justify-center flex-col space-y-5 text-center">
+        <h1 className="font-['Playfair_Display'] text-4xl font-semibold">
+          Get In Touch
+        </h1>
+        <p className="opacity-60">
+          Lorem ipsum dolor consectetuer adipiscing elit sed diam nonummy nibh
+          euismod tincidunt ut laoreet dolore magna <br /> aliquam erat volutpatcommodo
+          consequat.veniet quis?
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default InputSection;
--- client/src/pages/Contact/Contact.jsx
+++ client/src/pages/Contact/Contact.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import Hero from '../../components/ContactComponent/Hero';
-import AfterHero from '../../components/ContactComponent/AfterHero';
+import React from "react";
+import Hero from "../../components/ContactComponent/Hero";
+import AfterHero from "../../components/ContactComponent/AfterHero";
+import InputSection from "../../components/ContactComponent/InputSection";
 
 const Contact = () => {
   return (
-    <div>
+    <div className="space-y-20 [@media(max-width:576px)]:px-[15px] px-[50px] md:px-[60px] lg:px-[70px]">
       <div>
         <Hero />
       </div>
       <div>
         <AfterHero />
       </div>
+      <div>
+        <InputSection />
+      </div>
     </div>
   );
-}
+};
 
 export default Contact;
